a28c7ee1953968d14b41092930793e14b3a6bc2e
kurta17 <114169244+kurta17@users.noreply.github.com>
2023-10-13 10:57:05 +0700

    add car moving function

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,31 @@
+import turtle as t
+from random import random , randint , choice
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager():
+    def __init__(self):
+        self.allcars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+        self.create_car()
+
+    def create_car(self):
+        car = t.Turtle()
+        car.shape("square")
+        car.shapesize(stretch_wid=1,stretch_len=2)
+        car.color(choice(COLORS))
+        car.penup()
+        car.setheading(180)
+        car.goto(300,randint(-250,250))
+        self.allcars.append(car)
+
+    def car_move(self):
+        for i in self.allcars:
+            i.forward(self.car_speed)
+
+    
+
+
+    
