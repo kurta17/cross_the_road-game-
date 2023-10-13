ad1d81e2aa755f0597043bfa35dee5bb079d6a23
kurta17 <114169244+kurta17@users.noreply.github.com>
2023-10-14 00:29:50 +0700

    write some code which is nessesary to play game

--- main.py
+++ main.py
@@ -22,21 +22,27 @@ screen.listen()
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(0.05)
     screen.update()
     level.print_level()
     cars.car_move()
+    cars.create_car()
     if cars.crash_car(player_xpos=player.xcor(), player_ypos=player.ycor()) == True:
         screen.tracer(1)
         level.game_over(screen.update)
         player.go_start_pos()
         screen.tracer(0)
+        cars.reset_cars()
+        level.reset_level()
         screen.update()
+        
 
-    if player.ycor() > 300:
+    elif player.ycor() > 300:
         level.level += 1
         level.print_level()
+        cars.reset_cars()
         player.go_start_pos()
+        cars.incrise_speed()
 
 screen.exitonclick()
 
