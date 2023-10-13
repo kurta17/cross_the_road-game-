fea2d7b7e3870d57dc061f220e6bc51aa26d5d73
kurta17 <114169244+kurta17@users.noreply.github.com>
2023-10-13 10:26:22 +0700

    add print level function

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,21 @@
+from turtle import Turtle
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 0
+        self.penup()
+        self.color("black")
+        self.goto(-200,250)
+        self.hideturtle()
+
+
+    def print_level(self):
+        self.clear()
+        self.write(f"LEVEL: {self.level}", align="center", font=FONT)
+        
+
+    
+    
