6778fff81f9201c8fb9c5ef8588e8f89e4e4bac1
kurta17 <114169244+kurta17@users.noreply.github.com>
2023-10-14 00:28:24 +0700

    add reset car and incrise speed function

--- car_manager.py
+++ car_manager.py
@@ -9,28 +9,41 @@ class CarManager():
     def __init__(self):
         self.allcars = []
         self.car_speed = STARTING_MOVE_DISTANCE
+        self.move_count = 0
         self.create_car()
-        
-
-    def create_car(self):
-        car = t.Turtle()
-        car.shape("square")
-        car.shapesize(stretch_wid=1,stretch_len=2)
-        car.color(choice(COLORS))
-        car.penup()
-        car.setheading(180)
-        car.goto(300,randint(-250,250))
-        self.allcars.append(car)
+    
 
     def car_move(self):
         for i in self.allcars:
-            i.forward(self.car_speed)
+            i.forward(self.car_speed)  
+            self.move_count += 1
+
+    def create_car(self):
+        if self.move_count % 10 == 0:
+            car = t.Turtle()
+            car.shape("square")
+            car.shapesize(stretch_wid=1,stretch_len=2)
+            car.color(choice(COLORS))
+            car.penup()
+            car.setheading(180)
+            car.goto(300,randint(-250,250))
+            self.allcars.append(car)
 
     def crash_car(self, player_xpos, player_ypos):
         for i in self.allcars:
-            if - 20 < player_ypos - i.ycor() and -30 < player_xpos - i.xcor() < 30:
+            if - 20 < i.ycor()  - player_ypos < 20  and -30 < i.xcor() - player_xpos  < 30:
                 return True
         return False
+    
+    def reset_cars(self):
+        for car in self.allcars:
+            car.hideturtle()
+        self.allcars.clear()
+        self.car_speed = STARTING_MOVE_DISTANCE
+        self.move_count = 0
+
+    def incrise_speed(self):
+        self.car_speed += 5
                 
 
         
